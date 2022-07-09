689bcd5d63e26379b465446acfcc6dadcbc6a7ac
jjas420 <ayosuajon4@gmail.com>
2022-07-09 15:51:33 -0400

    clase nueva

--- src/miniproyecto/asd.java
+++ src/miniproyecto/asd.java
@@ -9,13 +9,6 @@ package miniproyecto;
  *
  * @author ayosu
  */
-public class Miniproyecto {
-
-    /**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
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("hola mundo 2");
-    }
+public class asd {
     
 }
