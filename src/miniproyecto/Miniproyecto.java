c919d43e58b2946ef6a0082d1682418413191f81
jjas420 <ayosuajon4@gmail.com>
2022-07-09 15:08:58 -0400

    hola mundo 2

--- src/miniproyecto/Miniproyecto.java
+++ src/miniproyecto/Miniproyecto.java
@@ -15,7 +15,7 @@ public class Miniproyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo 2");
     }
     
 }
